136b96b30a49749f453d003e25101b0f51da96f8
dimidroll450 <dmitriykvas@outlook.com>
2021-02-09 16:19:58 +0200

    add linter

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-import { getUserName } from './cli.js';
+import * as getUserName from './cli.js';
 
 console.log('Welcome to the Brain Games');
 
--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,6 @@
 import readlincSync from 'readline-sync';
 
-export const getUserName = () => {
+export default () => {
   const name = readlincSync.question('May I know you name?');
   console.log(`Hello, ${name}`);
 };
