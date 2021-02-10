a41d97e8a9e85de26b96ae65ccf422d28c4647a8
dimidroll450 <dmitriykvas@outlook.com>
2021-02-10 22:20:08 +0200

    fix brain-prime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -4,7 +4,7 @@ import { createRandomNumber, isPrime } from '../utils.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const gameLogic = () => {
-  const number = createRandomNumber(100, 1);
+  const number = createRandomNumber(100, 2);
   const correctResult = isPrime(number) ? 'yes' : 'no';
   const question = String(number);
   return { question, correctResult };
